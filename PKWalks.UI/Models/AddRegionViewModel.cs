0f1e8cdc4e01d2e725e90f3b53435c0097c888e9
Praveen Bommu <praveenbwebdev@gmail.com>
2025-05-27 23:48:43 +0530

    removed UI project

--- PKWalks.UI/Models/AddRegionViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PKWalks.UI.Models
-{
-    public class AddRegionViewModel
-    {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string RegionImageUrl { get; set; }
-    }
-}
